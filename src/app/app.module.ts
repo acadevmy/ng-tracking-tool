5bc4cfe46835dec51089f7f243e73e2ec66f66ad
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-10-07 14:48:52 +0200

    feat: add filter form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchFilterPipe } from './pipes/search-filter.pipe';
 import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    SearchFilterPipe
   ],
   imports: [
     BrowserModule
