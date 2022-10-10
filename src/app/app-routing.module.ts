cb9291dde69dbd48a75f6f9c33917270bf82b057
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-10-10 16:49:06 +0200

    feat: add projects detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ProjectContainerComponent } from "./projects/project-container/project-container.component";
+import { ProjectDetailComponent } from "./projects/project-detail/project-detail.component";
 
 const routes: Routes = [
+  { path: 'projects/detail', component: ProjectDetailComponent },
   { path: 'projects', component: ProjectContainerComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
